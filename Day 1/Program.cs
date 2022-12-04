0155c40f6f88d75d36ae84e91901a12a4e79f887
Toby Maynard <t.maynard-2020@hull.ac.uk>
2022-12-04 02:15:10 +0000

    Completed Day 1 challenge. Contains bug - last elf is not counted. Will fix later as its past my bed time
    https://adventofcode.com/2022/day/1

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -7,7 +7,7 @@ namespace Day_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             List<string> cals = File.ReadAllText(@"C:\\Users\\Toby\\source\repos\\Advent Of Code 2022\\Day 1\\input.txt").Split("\n\n").ToList();
             
@@ -16,12 +16,11 @@ namespace Day_1
             {
                 elfCallories.Add(cals[i].Split("\n").Select(int.Parse).ToArray().Sum());
             }
-            var top3 = elfCallories.GroupBy(x => x)
-                 .Select(x => x.OrderByDescending(y => y).First())
-                 .OrderByDescending(x => x)
-                 .ThenBy(x => x)
-                 .Take(3).Sum(); //Take 1 for part 1 answer 
-            Console.WriteLine(top3);
+            
+            int TOTAL = elfCallories.OrderByDescending(i => i)
+                .Take(3)
+                .Sum();
+            Console.WriteLine(TOTAL);
         }
     }
 }
