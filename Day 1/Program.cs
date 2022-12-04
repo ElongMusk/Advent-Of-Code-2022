54b474b1e358c500f0070b4b787e3ab0209e8e97
Toby Maynard <t.maynard-2020@hull.ac.uk>
2022-12-04 02:01:29 +0000

    Add project files.

---
+++ Day 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cals = File.ReadAllText(@"C:\\Users\\Toby\\source\repos\\Advent Of Code 2022\\Day 1\\input.txt").Split("\n\n").ToList();
+            
+            List<int> elfCallories = new List<int>();
+            for (int i = 0; i < cals.Count - 1; i++)
+            {
+                elfCallories.Add(cals[i].Split("\n").Select(int.Parse).ToArray().Sum());
+            }
+            var top3 = elfCallories.GroupBy(x => x)
+                 .Select(x => x.OrderByDescending(y => y).First())
+                 .OrderByDescending(x => x)
+                 .ThenBy(x => x)
+                 .Take(3).Sum(); //Take 1 for part 1 answer 
+            Console.WriteLine(top3);
+        }
+    }
+}
