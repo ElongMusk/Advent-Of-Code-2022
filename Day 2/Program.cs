5141cedc862bef228ec305c3c830521dfc757fbf
CrimsonTome <crimsontome427@protonmail.com>
2022-12-04 19:37:29 +0000

    Day 2 second start done

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -9,11 +9,12 @@ namespace Day_2
     {
         static void Main(string[] args)
         {
-            List<string> games = File.ReadAllText(@"C:\\Users\\Toby\\source\repos\\Advent Of Code 2022\\Day 2\\input.txt")
-                .Split("\n")
+            List<string> games = File.ReadAllText(@"C:\Users\Toby\Source\Repos\Advent-Of-Code-2022\Day 2\input.txt")
+                .Split("\r\n")
                 .ToList();
 
-            int totalPoints = 0;
+            int totalPoints1 = 0;
+            int totalPoints2 = 0;
             foreach (var game in games)
             {
                 if(game != "")
@@ -21,12 +22,12 @@ namespace Day_2
                     char yourChoice = game[^1],
                     oppositionChoice = game[0];
 
-                    totalPoints += CalcualteRoundScore(yourChoice, oppositionChoice);
+                    totalPoints1 += CalcualteRoundScore(yourChoice, oppositionChoice);
+                    totalPoints2 += CalculateRoundChoice(yourChoice, oppositionChoice);
                 }
-                
             }
 
-            Console.WriteLine(totalPoints);
+            Console.WriteLine(totalPoints1 + " " + totalPoints2);
         }
 
         static int CalcualteRoundScore(char myChoice, char opChoice)
@@ -68,6 +69,48 @@ namespace Day_2
                     }
                     return score;
             }
+            return 0;
+        }
+
+
+        static int CalculateRoundChoice(char myChoice, char opChoice)
+        {
+            int score = 0;
+            switch (opChoice)
+            {
+                case 'A':
+                    switch (myChoice)
+                    {
+                        case 'X': return score += 3; 
+
+                        case 'Y': return score += 4;
+
+                        case 'Z': return score += 8;
+                    }
+                    break;
+                case 'B':
+                    switch (myChoice)
+                    {
+                        case 'X': return score += 1;
+
+                        case 'Y': return score += 5;
+
+                        case 'Z': return score += 9;
+                    }
+                    break;
+                case 'C':
+                    switch (myChoice)
+                    {
+                        case 'X': return score += 2;
+
+                        case 'Y': return score += 6;
+
+                        case 'Z': return score += 7;
+                    }
+                    return score;
+            }
+
+
             return 0;
         }
     }
